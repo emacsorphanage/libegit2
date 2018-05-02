7c9a153a94abdf6c50b1be9522c1ca766e3d8564
Eivind Fonn <evfonn@gmail.com>
2018-05-02 14:00:06 +0200

    Integrate Appveyor CI

---
+++ .appveyor-script.ps1
@@ -0,0 +1,15 @@
+# For some reason, the return code of emacs isn't correctly communicated back to the script.
+# Instead, we store the stdout/stderr and grep for "FAILED".
+function Run-Test {
+    param( $TestName )
+    $command = "emacs --batch -L build -L test -l test-helper -l $($TestName)-test -f ert-run-tests-batch-and-exit"
+    $output = Invoke-Expression "$command 2>&1"
+    Write-Host $output
+    if ($output | select-string -Pattern "FAILED") {
+        exit 1
+    }
+}
+
+Run-Test -TestName "refcount"
+Run-Test -TestName "reference"
+Run-Test -TestName "repository"
