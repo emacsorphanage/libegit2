b51009b6364c35791d51bb3dbe45da9298a99a96
Scott Dubinsky <smdubinsky@gmail.com>
2018-10-11 13:13:12 +0300

    fix method name - replace hyphen with underscore

--- src/egit-commit.h
+++ src/egit-commit.h
@@ -3,4 +3,4 @@
 #ifndef EGIT_COMMIT_H
 #define EGIT_COMMIT_H
 
-EGIT_DEFUN(libgit-commit-create, emacs_value _repo, emacs_value _update_ref, emacs_value _author, emacs_value _committer, emacs_value _message_encoding, emacs_value _message, emacs_value _tree, emacs_value _parent_count, emacs_value _parents);
+EGIT_DEFUN(libgit_commit_create, emacs_value _repo, emacs_value _update_ref, emacs_value _author, emacs_value _committer, emacs_value _message_encoding, emacs_value _message, emacs_value _tree, emacs_value _parent_count, emacs_value _parents);
