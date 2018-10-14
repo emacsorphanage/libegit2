d48bc2a41a2080783a21d16012c2c7152648aeb9
Scott Dubinsky <smdubinsky@gmail.com>
2018-10-14 13:25:11 +0300

    fix variable name

--- src/egit-signature.h
+++ src/egit-signature.h
@@ -3,6 +3,6 @@
 #ifndef EGIT_SIGNATURE_H
 #define EGIT_SIGNATURE_H
 
-EGIT_DEFUN(signature_default, emacs_value repo)
+EGIT_DEFUN(signature_default, emacs_value _repo);
 
 #endif /* EGIT_SIGNATURE_H */
