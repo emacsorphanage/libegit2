0b74d945a53bb4c194710d91a7fbc2343e44cd90
Andrew Savonichev <andrew.savonichev@gmail.com>
2018-10-15 19:42:30 +0300

    Add annotated_commit_from_ref

--- src/egit-annotated-commit.c
+++ src/egit-annotated-commit.c
@@ -4,3 +4,21 @@
 #include "interface.h"
 #include "egit-repository.h"
 
+EGIT_DOC(annotated_commit_from_ref, "REPO REF",
+         "Return an annotated commit for the given REF.");
+emacs_value egit_annotated_commit_from_ref(emacs_env *env,
+                                           emacs_value _repo,
+                                           emacs_value _ref)
+{
+    EGIT_ASSERT_REPOSITORY(_repo);
+    EGIT_ASSERT_REFERENCE(_ref);
+
+    git_repository *repo = EGIT_EXTRACT(_repo);
+    git_reference *ref = EGIT_EXTRACT(_ref);
+
+    git_annotated_commit *ann_commit = NULL;
+    int retval = git_annotated_commit_from_ref(&ann_commit, repo, ref);
+    EGIT_CHECK_ERROR(retval);
+
+    return egit_wrap(env, EGIT_ANNOTATED_COMMIT, ann_commit, NULL);
+}
--- test/annotated-commit-test.el
+++ test/annotated-commit-test.el
@@ -1,1 +1,8 @@
-
+(ert-deftest annotated-commit-from-ref ()
+  (with-temp-dir path
+    (init)
+    (commit-change "test" "foo")
+    (let* ((repo (libgit-repository-open path))
+           (ref  (libgit-reference-lookup repo "HEAD"))
+           (ann  (libgit-annotated-commit-from-ref repo ref)))
+      (should (libgit-annotated-commit-p ann)))))
