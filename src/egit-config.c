8f58e2c339470e01fad135bf1b13b48f6dc631d5
Eivind Fonn <evfonn@gmail.com>
2018-11-06 13:27:05 +0100

    Add libgit-config-get-string

--- src/egit-config.c
+++ src/egit-config.c
@@ -18,3 +18,23 @@ emacs_value egit_config_snapshot(emacs_env *env, emacs_value _config)
     EGIT_CHECK_ERROR(retval);
     return egit_wrap(env, EGIT_CONFIG, snapshot);
 }
+
+
+// =============================================================================
+// Getters
+
+EGIT_DOC(config_get_string, "CONFIG NAME",
+         "Get the value of NAME in CONFIG as a string.\n"
+         "CONFIG must be a snapshot, see `libgit-config-snapshot'.");
+emacs_value egit_config_get_string(emacs_env *env, emacs_value _config, emacs_value _name)
+{
+    EGIT_ASSERT_CONFIG(_config);
+    EGIT_ASSERT_STRING(_name);
+    git_config *config = EGIT_EXTRACT(_config);
+    char *name = EGIT_EXTRACT_STRING(_name);
+    const char *value;
+    int retval = git_config_get_string(&value, config, name);
+    free(name);
+    EGIT_CHECK_ERROR(retval);
+    return env->make_string(env, value, strlen(value));
+}
--- src/egit-config.h
+++ src/egit-config.h
@@ -5,4 +5,6 @@
 
 EGIT_DEFUN(config_snapshot, emacs_value _config);
 
+EGIT_DEFUN(config_get_string, emacs_value _config, emacs_value _name);
+
 #endif /* EGIT_CONFIG_H */
