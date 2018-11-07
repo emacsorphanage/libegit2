6fd53e80a9b497488b6d3ebd3c77babe70373f0e
Eivind Fonn <evfonn@gmail.com>
2018-11-07 09:22:30 +0100

    Add libgit-tree-{lookup,owner}

--- src/egit-tree.c
+++ src/egit-tree.c
@@ -6,14 +6,44 @@
 
 
 // =============================================================================
+// Constructors
+
+EGIT_DOC(tree_lookup, "REPO OID", "Look up a tree in REPO by OID.");
+emacs_value egit_tree_lookup(emacs_env *env, emacs_value _repo, emacs_value _oid)
+{
+    EGIT_ASSERT_REPOSITORY(_repo);
+    EGIT_ASSERT_STRING(_oid);
+
+    git_repository *repo = EGIT_EXTRACT(_repo);
+    git_oid oid;
+    EGIT_EXTRACT_OID(_oid, oid);
+
+    git_tree *tree;
+    int retval = git_tree_lookup(&tree, repo, &oid);
+    EGIT_CHECK_ERROR(retval);
+
+    return egit_wrap(env, EGIT_TREE, tree);
+}
+
+
+// =============================================================================
 // Getters
 
 EGIT_DOC(tree_id, "TREE", "Return the ID of TREE.");
 emacs_value egit_tree_id(emacs_env *env, emacs_value _tree)
 {
     EGIT_ASSERT_TREE(_tree);
-    git_commit *tree = EGIT_EXTRACT(_tree);
+    git_tree *tree = EGIT_EXTRACT(_tree);
     const git_oid *oid = git_tree_id(tree);
     const char *oid_s = git_oid_tostr_s(oid);
     return env->make_string(env, oid_s, strlen(oid_s));
+}
+
+EGIT_DOC(tree_owner, "TREE", "Return the repository that TREE belongs to.");
+emacs_value egit_tree_owner(emacs_env *env, emacs_value _tree)
+{
+    EGIT_ASSERT_TREE(_tree);
+    git_tree *tree = EGIT_EXTRACT(_tree);
+    git_repository *repo = git_tree_owner(tree);
+    return egit_wrap_repository(env, repo);
 }
--- src/egit-tree.h
+++ src/egit-tree.h
@@ -3,6 +3,9 @@
 #ifndef EGIT_TREE_H
 #define EGIT_TREE_H
 
+EGIT_DEFUN(tree_lookup, emacs_value _repo, emacs_value _oid);
+
 EGIT_DEFUN(tree_id, emacs_value _tree);
+EGIT_DEFUN(tree_owner, emacs_value _tree);
 
 #endif /* EGIT_TREE_H */
