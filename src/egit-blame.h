29c80f5765c7b827914ae51c4de2c584065d485d
Eivind Fonn <evfonn@gmail.com>
2018-11-13 14:46:30 +0100

    blame: Return opaque blame-hunk objects instead of alists

--- src/egit-blame.h
+++ src/egit-blame.h
@@ -8,4 +8,10 @@ EGIT_DEFUN(blame_get_hunk_byindex, emacs_value _blame, emacs_value _index);
 EGIT_DEFUN(blame_get_hunk_byline, emacs_value _blame, emacs_value _line);
 EGIT_DEFUN(blame_get_hunk_count, emacs_value _blame);
 
+EGIT_DEFUN(blame_hunk_commit_id, emacs_value _hunk, emacs_value orig);
+EGIT_DEFUN(blame_hunk_lines, emacs_value _hunk);
+EGIT_DEFUN(blame_hunk_orig_path, emacs_value _hunk);
+EGIT_DEFUN(blame_hunk_signature, emacs_value _hunk, emacs_value orig);
+EGIT_DEFUN(blame_hunk_start_line_number, emacs_value _hunk, emacs_value orig);
+
 #endif /* EGIT_BLAME_H */
