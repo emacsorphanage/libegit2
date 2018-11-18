84512b8f6fac675138ac839425b61848e834c7ff
Eivind Fonn <evfonn@gmail.com>
2018-11-18 10:59:28 +0100

    Add libgit-submodule-open

--- src/egit-submodule.h
+++ src/egit-submodule.h
@@ -11,6 +11,7 @@ EGIT_DEFUN(submodule_ignore, emacs_value _sub);
 EGIT_DEFUN(submodule_index_id, emacs_value _sub);
 EGIT_DEFUN(submodule_location, emacs_value _sub, emacs_value flag);
 EGIT_DEFUN(submodule_name, emacs_value _sub);
+EGIT_DEFUN(submodule_open, emacs_value _sub);
 EGIT_DEFUN(submodule_owner, emacs_value _sub);
 EGIT_DEFUN(submodule_path, emacs_value _sub);
 EGIT_DEFUN(submodule_status, emacs_value _repo, emacs_value _name, emacs_value _ignore, emacs_value flag);
