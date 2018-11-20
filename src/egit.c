b86fa7d8166962c46086f2fba34e0bb8be1d0ba2
Eivind Fonn <evfonn@gmail.com>
2018-11-20 11:10:20 +0100

    Add libgit-blob-owner

--- src/egit.c
+++ src/egit.c
@@ -540,6 +540,7 @@ void egit_init(emacs_env *env)
     DEFUN("libgit-blob-binary-p", blob_binary_p, 1, 1);
     DEFUN("libgit-blob-filtered-content", blob_filtered_content, 2, 3);
     DEFUN("libgit-blob-id", blob_id, 1, 1);
+    DEFUN("libgit-blob-owner", blob_owner, 1, 1);
     DEFUN("libgit-blob-rawcontent", blob_rawcontent, 1, 1);
     DEFUN("libgit-blob-rawsize", blob_rawsize, 1, 1);
 
