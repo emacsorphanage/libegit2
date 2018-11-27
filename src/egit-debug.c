484ac567a2965c55b15d04ab7483717cfa402bbb
Eivind Fonn <evfonn@gmail.com>
2018-11-27 15:46:39 +0100

    Add debug functions for keeping track of allocs and frees

--- src/egit-debug.c
+++ src/egit-debug.c
@@ -3,6 +3,59 @@
 #include "egit-debug.h"
 
 
+typedef struct ptr_cell_s ptr_cell;
+
+struct ptr_cell_s {
+    void *ptr;
+    ptr_cell *next;
+};
+
+static ptr_cell *alloc_stack = NULL;
+static ptr_cell *finalize_stack = NULL;
+static ptr_cell *free_stack = NULL;
+
+static void push_stack(ptr_cell **stack, void *ptr)
+{
+    ptr_cell *cell = (ptr_cell*) malloc(sizeof(ptr_cell));
+    cell->ptr = ptr;
+    cell->next = *stack;
+    *stack = cell;
+}
+
+static void *pop_stack(ptr_cell **stack)
+{
+    ptr_cell *cell = *stack;
+    if (!cell)
+        return NULL;
+
+    *stack = cell->next;
+    void *retval = cell->ptr;
+    free(cell);
+    return retval;
+}
+
+static emacs_value convert_stack(emacs_env *env, ptr_cell **stack)
+{
+    emacs_value list = em_nil;
+    void *ptr;
+    while ((ptr = pop_stack(stack)))
+        list = em_cons(env, EM_INTEGER((ptrdiff_t) ptr), list);
+    return list;
+}
+
+void egit_signal_alloc(void *ptr) { push_stack(&alloc_stack, ptr); }
+void egit_signal_finalize(void *ptr) { push_stack(&finalize_stack, ptr); }
+void egit_signal_free(void *ptr) { push_stack(&free_stack, ptr); }
+
+EGIT_DOC(_allocs, "", "Return a list of wrapper pointers that have been allocated.");
+emacs_value egit__allocs(emacs_env *env) { return convert_stack(env, &alloc_stack); }
+
+EGIT_DOC(_finalizes, "", "Return a list of wrapper pointers that have been finalized.");
+emacs_value egit__finalizes(emacs_env *env) { return convert_stack(env, &finalize_stack); }
+
+EGIT_DOC(_frees, "", "Return a list of wrapper pointers that have been freed.");
+emacs_value egit__frees(emacs_env *env) { return convert_stack(env, &free_stack); }
+
 EGIT_DOC(_refcount, "OBJ", "Return the reference count of OBJ.");
 emacs_value egit__refcount(emacs_env *env, emacs_value val)
 {
