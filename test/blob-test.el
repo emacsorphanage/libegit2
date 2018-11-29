dda4f55a727ef37da8ac1c0962eccde3b6386ded
Eivind Fonn <evfonn@gmail.com>
2018-11-29 20:59:25 +0100

    Add blob-create tests

--- test/blob-test.el
+++ test/blob-test.el
@@ -33,3 +33,30 @@
         (should (string= str (libgit-blob-rawcontent blob)))
         (should (string= "" (libgit-blob-filtered-content blob "filename")))
         (should (string= str (libgit-blob-filtered-content blob "filename" t)))))))
+
+(ert-deftest blob-create-fromdisk ()
+  (with-temp-dir path
+    (init)
+    (write "file" "here's some text\n")
+    (let* ((repo (libgit-repository-open path))
+           (id (libgit-blob-create-fromdisk repo (concat path "file"))))
+      (should (string= id "e11288449233d84c7dd74759fb6b20b06642b7c5"))
+      (let ((blob (libgit-blob-lookup repo id)))
+        (should (libgit-blob-p blob))
+        (should-not (multibyte-string-p (libgit-blob-rawcontent blob)))
+        (should (string= "here's some text\n" (libgit-blob-rawcontent blob)))))))
+
+(ert-deftest blob-create-fromstring ()
+  (with-temp-dir path
+    (init)
+    (let* ((repo (libgit-repository-open path))
+           ;; Valid utf-8 multibyte sequences, to check that we do not
+           ;; get a multibyte string back
+           (cont (unibyte-string ?\x24 ?\xc2 ?\xa2 ?\xe0 ?\xa4 ?\xb9
+                                 ?\xe2 ?\x82 ?\xac ?\xf0 ?\x90 ?\x8d ?\x88))
+           (id (libgit-blob-create-fromstring repo cont)))
+      (should (string= id "86ace23e965f5c8bb77f31ce58ed0d2e34d1152e"))
+      (let ((blob (libgit-blob-lookup repo id)))
+        (should (libgit-blob-p blob))
+        (should-not (multibyte-string-p (libgit-blob-rawcontent blob)))
+        (should (equal cont (libgit-blob-rawcontent blob)))))))
