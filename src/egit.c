bf0810855801c011e4af82099446b54d52cc6bd6
Eivind Fonn <evfonn@gmail.com>
2018-11-29 23:09:56 +0100

    Add libgit-treebuilder-remove

--- src/egit.c
+++ src/egit.c
@@ -833,4 +833,5 @@ void egit_init(emacs_env *env)
     DEFUN("libgit-treebuilder-entrycount", treebuilder_entrycount, 1, 1);
     DEFUN("libgit-treebuilder-get", treebuilder_get, 2, 2);
     DEFUN("libgit-treebuilder-clear", treebuilder_clear, 1, 1);
+    DEFUN("libgit-treebuilder-remove", treebuilder_remove, 2, 2);
 }
