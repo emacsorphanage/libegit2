a921bd9e0f8d5cf985d17c78633d1b907c8a4f1b
Eivind Fonn <evfonn@gmail.com>
2018-11-30 17:15:35 +0100

    Add libgit-revparse

--- src/egit-revparse.h
+++ src/egit-revparse.h
@@ -3,6 +3,7 @@
 #ifndef EGIT_REVPARSE_H
 #define EGIT_REVPARSE_H
 
+EGIT_DEFUN(revparse, emacs_value _repo, emacs_value _spec);
 EGIT_DEFUN(revparse_ext, emacs_value _repo, emacs_value _spec);
 EGIT_DEFUN(revparse_single, emacs_value _repo, emacs_value _spec);
 
