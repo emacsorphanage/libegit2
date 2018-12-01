7b497b736e606757597fa0ccb874a19f9deb4d40
Andrew Savonichev <andrew.savonichev@gmail.com>
2018-12-01 14:06:55 +0300

    Add libgit-reset-default

--- src/egit-reset.h
+++ src/egit-reset.h
@@ -15,4 +15,9 @@ EGIT_DEFUN(reset_from_annotated,
            emacs_value reset_type,
            emacs_value checkout_opts);
 
+EGIT_DEFUN(reset_default,
+           emacs_value repo,
+           emacs_value target,
+           emacs_value pathspecs);
+
 #endif /* EGIT_RESET_H */
--- test/reset-test.el
+++ test/reset-test.el
@@ -108,3 +108,27 @@
   		       (libgit-annotated-commit-id reset-to)))
       (should (string= (read-file "a") "content-a1"))
       (should (eq nil (libgit-status-file repo "a"))))))
+
+(ert-deftest reset-default ()
+  (with-temp-dir path
+    (init)
+    (commit-change "a" "content-a1")
+    (commit-change "b" "content-b1")
+    (let* ((repo (libgit-repository-open path))
+	   (reset-to
+	    (libgit-object-lookup
+	     repo (libgit-reference-name-to-id repo "HEAD"))))
+
+      (write "a" "content-a2")
+      (write "b" "content-b2")
+      (add "a" "b")
+      (libgit-reset-default repo reset-to '("a"))
+
+      ;; Head should not move, files matching pathspec should become
+      ;; unstaged.
+      (should (string= (libgit-reference-name-to-id repo "HEAD")
+		       (libgit-object-id reset-to)))
+      (should (string= (read-file "a") "content-a2"))
+      (should (string= (read-file "b") "content-b2"))
+      (should (member 'wt-modified (libgit-status-file repo "a")))
+      (should (member 'index-modified (libgit-status-file repo "b"))))))
