5c16c0413f19beeb03e829d18e69acc905c57257
Eivind Fonn <evfonn@gmail.com>
2018-12-02 12:03:51 +0100

    Add libgit-submodule-set-url

--- src/egit.c
+++ src/egit.c
@@ -809,6 +809,7 @@ void egit_init(emacs_env *env)
     DEFUN("libgit-submodule-set-fetch-recurse-submodules", submodule_set_fetch_recurse_submodules, 2, 3);
     DEFUN("libgit-submodule-set-ignore", submodule_set_ignore, 3, 3);
     DEFUN("libgit-submodule-set-update", submodule_set_update, 3, 3);
+    DEFUN("libgit-submodule-set-url", submodule_set_url, 3, 3);
 
     // Tag
     DEFUN("libgit-tag-lookup", tag_lookup, 2, 2);
