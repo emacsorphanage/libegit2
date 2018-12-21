57a9f07725ff8215403dc453582294f490f40392
Eivind Fonn <evfonn@gmail.com>
2018-12-21 15:57:40 +0100

    index: Fix possible memory leak

--- src/egit-index.c
+++ src/egit-index.c
@@ -69,7 +69,10 @@ emacs_value egit_index_conflict_foreach(emacs_env *env, emacs_value _index, emac
         args[3] = egit_wrap(env, EGIT_INDEX_ENTRY, theirs, index_wrp);
         env->funcall(env, function, 4, args);
 
-        EM_RETURN_NIL_IF_NLE();
+        if (env->non_local_exit_check(env)) {
+            git_index_conflict_iterator_free(iter);
+            return esym_nil;
+        }
     }
 }
 
