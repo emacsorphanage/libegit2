116b58ac3b4bd611bd8757362345bcfbf2c7a924
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-05 18:55:27 +0800

    Delete test directory

--- test/ignore-test.el
+++
@@ -1,8 +0,0 @@
-(ert-deftest ignore ()
-  (with-temp-dir path
-    (init)
-    (let ((repo (libgit-repository-open path)))
-      (libgit-ignore-add-rule repo "testrule")
-      (should (eql t (libgit-ignore-path-ignored-p repo "testrule")))
-      (libgit-ignore-clear-internal-rules repo)
-      (should (eql nil (libgit-ignore-path-ignored-p repo "testrule"))))))
