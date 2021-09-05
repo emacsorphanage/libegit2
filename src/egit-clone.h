f2700b8e20d98ab67f24b3857cff83f3d41d63d4
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-05 18:54:28 +0800

    Delete src directory

--- src/egit-clone.h
+++
@@ -1,8 +0,0 @@
-#include "egit.h"
-
-#ifndef EGIT_CLONE_H
-#define EGIT_CLONE_H
-
-EGIT_DEFUN(clone, emacs_value _url, emacs_value _path);
-
-#endif /* EGIT_CLONE_H */
--- src/egit-transaction.h
+++
@@ -1,8 +0,0 @@
-#include "egit.h"
-
-#ifndef EGIT_TRANSACTION_H
-#define EGIT_TRANSACTION_H
-
-EGIT_DEFUN(transaction_commit, emacs_value _trans);
-
-#endif /* EGIT_TRANSACTION_H */
